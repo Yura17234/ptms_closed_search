2151c42bd95c4ddc098c868627b5478b86a0cc84
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-06-26 02:18:36 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="test_module",
+    name="ptm_search",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],  # сюда добавь зависимости при необходимости
     entry_points={
         'console_scripts': [
-            'run_prepare_ptm_search = test_module.run_prepare_ptm_search:main',
-            'run_multiple_search = test_module.run_multiple_search:main',
-            'run_aggregate_results = test_module.run_aggregate_results:main',
+            'run_prepare_ptm_search = ptm_search.run_prepare_ptm_search:main',
+            'run_multiple_search = ptm_search.run_multiple_search:main',
+            'run_aggregate_results = ptm_search.run_aggregate_results:main',
         ],
     },
 )
