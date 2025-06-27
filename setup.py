fbd74583e902bdb2ac1ae3c9a6b41f53fc633e11
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-06-27 21:57:58 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(
     },
     install_requires=[
         "pandas>=1.0",
+        "unipressed>=1.0.0",
+        "pyteomics>=4.0.0",
+        "configupdater>=1.0",
     ],
     entry_points={
         'console_scripts': [
