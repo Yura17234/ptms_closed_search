3eca8791dfe67aa5fe6e76822d0114d51da0813e
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-06-27 21:24:09 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(
     name="ptm_search",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "ptm_search": [
+            "data/*.json",
+            "data/*.json.gz",
+        ],
+    },
     install_requires=[
         "pandas>=1.0",
     ],
