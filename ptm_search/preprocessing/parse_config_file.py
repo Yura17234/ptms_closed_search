e885d540cca4ab5e55d67bb994f4b217b125814f
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-06-28 16:06:07 +0300

    Add files via upload

--- ptm_search/preprocessing/parse_config_file.py
+++ ptm_search/preprocessing/parse_config_file.py
@@ -12,7 +12,7 @@ import json
 '''
 
 # ---------------------------------------------------/ --- /------------------------------------------------------------
-def make_config_files(list_of_MOD_RES, config, variant_fo_search_):
+def make_config_files(list_of_MOD_RES, config, variant_of_search_):
     print(f'Список всех модификаций:\n{list_of_MOD_RES}\n')
 
     module_dir = Path(__file__).parent.parent.resolve()
@@ -24,13 +24,15 @@ def make_config_files(list_of_MOD_RES, config, variant_fo_search_):
     st_search_config = configparser.ConfigParser()
     st_search_config.sections()
     st_search_config.read(str(config.base_config_path))
+    print(config.base_config_path)
+    print(Path(config.base_config_path).exists())
 
     configs_full_search_dir = '_'
     configs_fast_search_dir = '_'
-    if variant_fo_search_ == 1:
+    if variant_of_search_ == 1:
         configs_full_search_dir = config.ptm_search_dir / f'{config.experiment_name}_Configs_full_search'
         os.makedirs(configs_full_search_dir, exist_ok=True)
-    elif variant_fo_search_ == 2:
+    elif variant_of_search_ == 2:
         configs_fast_search_dir = config.ptm_search_dir / f'{config.experiment_name}_Configs_fast_search'
         os.makedirs(configs_fast_search_dir, exist_ok=True)
 
@@ -54,9 +56,9 @@ def make_config_files(list_of_MOD_RES, config, variant_fo_search_):
         elif st_search_config["modifications"]["variable"] != '':
             updater["modifications"]["variable"].value = f'{st_search_config["modifications"]["variable"]}, {mification_name_for_Identipy}'
 
-        if variant_fo_search_ == 1: # Добавление полного пути к полной базе данных поиска
+        if variant_of_search_ == 1: # Добавление полного пути к полной базе данных поиска
             updater["input"]["database"].value = f'{str(config.ptm_search_dir)}/{config.experiment_name}_PTMs_search_{config.analysis_index}_reverse.fasta'
-        elif variant_fo_search_ == 2: # Добавление полного пути к малым базам данных поиска
+        elif variant_of_search_ == 2: # Добавление полного пути к малым базам данных поиска
             modif_for_fasta = modif.replace(' ', '_').replace(';', '').replace('/', '_')
             updater["input"]["database"].value = f'{str(config.ptm_search_dir)}/{config.experiment_name}_fasta_for_fast_search/{modif_for_fasta}_{config.analysis_index}_reverse.fasta'
 
@@ -67,10 +69,10 @@ def make_config_files(list_of_MOD_RES, config, variant_fo_search_):
 
         # Сохранение config-файла
         modif2 = modif.replace(' ', '_').replace(';', '').replace('/', '_')
-        if variant_fo_search_ == 1:
+        if variant_of_search_ == 1:
             with open(configs_full_search_dir / f'{modif2}_{config.analysis_index}.cfg', 'w') as file:
                 file.write(str(updater))
-        if variant_fo_search_ == 2:
+        if variant_of_search_ == 2:
             with open(configs_fast_search_dir / f'{modif2}_{config.analysis_index}.cfg', 'w') as file:
                 file.write(str(updater))
 
@@ -82,8 +84,8 @@ def make_config_files(list_of_MOD_RES, config, variant_fo_search_):
     print(f'Число модификаций {len(list_of_MOD_RES)}')
 
 # ---------------------/ Открытие необходимых файлов на чтение и запись. Запуск внутренных функций /--------------------
-def parse_config_file(list_of_MOD_RES_, config, variant_fo_search):
+def parse_config_file(list_of_MOD_RES_, config, variant_of_search):
     text4 = ' Создание конфигурационных файлов для поиска PTM '
     number4 = int(round((200 - len(text4)) / 2, 0))
     print(f'{text4:.^{number4}}')
-    make_config_files(list_of_MOD_RES_, config, variant_fo_search)
+    make_config_files(list_of_MOD_RES_, config, variant_of_search)
