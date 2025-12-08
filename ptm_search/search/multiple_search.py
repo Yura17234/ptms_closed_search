3d35763c2e434eb1dd4ec8dac7e747e6e1b4cf14
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-12-09 01:39:22 +0300

    Add files via upload

--- ptm_search/search/multiple_search.py
+++ ptm_search/search/multiple_search.py
@@ -16,7 +16,7 @@ def multiple_search(config) -> NoReturn:
     results_dir.mkdir(exist_ok=True)
 
     config_files = sorted(configs_dir.glob("*.cfg"))
-    logger.info(f'\nНайдено конфигураций:\n{len(config_files)}\n')
+    logger.info(f'\nConfigurations found:\n{len(config_files)}\n')
 
     already_done = {d.stem.split('_result')[0] for d in results_dir.iterdir() if d.is_file()}
 
@@ -28,7 +28,7 @@ def multiple_search(config) -> NoReturn:
 
         logger.info(f"{'':-^{50}}\n{count}/{len(config_files)} | {mod_name}\n{'':-^{50}}")
 
-        # --- Запуск IdentiPy ---
+        # start identipy
         # os.system(f'identipy {str(config.work_dir)}/*.mgf -cfg {str(cfg_path)}')
         mgf_files = [str(mgf) for mgf in list(config.work_dir.glob('*.mgf'))]
         identipy_cmd = ["identipy"] + mgf_files + ["-cfg", str(cfg_path)]
@@ -36,10 +36,10 @@ def multiple_search(config) -> NoReturn:
         try:
             subprocess.run(identipy_cmd, check=True)
         except subprocess.CalledProcessError as e:
-            logger.error(f"\nОшибка при запуске IdentiPy:\n{e}")
+            logger.error(f"\nError when starting IdentiPy:\n{e}")
             continue
 
-        # --- Сбор результатов .pep.xml ---
+        # collecting the results from .pep.xml files
         ptm_all_df = pd.DataFrame()
         for pepxml_file in config.work_dir.glob("*.pep.xml"):
             file_name = pepxml_file.stem
@@ -47,10 +47,10 @@ def multiple_search(config) -> NoReturn:
             df_pep['file_name'] = file_name
             ptm_all_df = pd.concat([ptm_all_df, df_pep], ignore_index=True)
 
-        # --- Сохраняем результаты ---
+        # saving the results
         result_csv_path = results_dir / f'{mod_name}_result.csv'
         ptm_all_df.to_csv(result_csv_path, index=False)
 
-        # Удаление .pep.xml
+        # deletion .pep.xml files
         for f in config.work_dir.glob("*.pep.xml"):
             f.unlink()
