edc1946ea607fd02e25071b2ca748383f940a61a
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-12-09 22:47:54 +0300

    Add files via upload

--- ptm_search/preprocessing/prepare_ptm_search.py
+++ ptm_search/preprocessing/prepare_ptm_search.py
@@ -48,7 +48,7 @@ def prepare_ptm_search(config) -> NoReturn:
     logger.info(f'The number of proteins from the standard initial search: {len(list(acc_to_names_dict.keys()))}')
     grouped_prots_by_ptms_dict = adding_ptm_info_from_db_ptm(grouped_prots_by_ptms_dict, list(acc_to_names_dict.keys()))
 
-    if config.additional_lists_path != '':
+    if '.txt' in config.additional_lists_path:
         grouped_prots_by_ptms_dict = adding_ptm_info_from_additional_lists(grouped_prots_by_ptms_dict,
                                                                  list(acc_to_names_dict.keys()), config)
 
