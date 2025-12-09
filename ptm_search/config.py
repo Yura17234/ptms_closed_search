cbbb771b0ff3c55ae5e9256acf2c716588818227
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2025-12-09 23:36:26 +0300

    Add files via upload

--- ptm_search/config.py
+++ ptm_search/config.py
@@ -25,7 +25,8 @@ class Config:
         self.uniprot_query_path = Path(os.path.expanduser(uniprot_query_path)).resolve()
         raw_base_config_path = self._config.get('paths', 'base_config_path')
         self.base_config_path = Path(os.path.expanduser(raw_base_config_path)).resolve()
-        if str(self._config.get('paths', 'additional_lists_path')) == '0':
+        print(self._config.get('paths', 'additional_lists_path'), type(self._config.get('paths', 'additional_lists_path')))
+        if self._config.get('paths', 'additional_lists_path') == 0:
             self.additional_lists_path = ''
         else:
             raw_additional_lists_path = self._config.get('paths', 'additional_lists_path')
