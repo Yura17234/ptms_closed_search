3d9c11ee1350011405b1ade885dcf98924b756f7
Yury Strogov <97240988+Yura17234@users.noreply.github.com>
2026-02-09 00:50:37 +0300

    Add files via upload

--- ptm_search/postprocessing/analysis_of_result.py
+++ ptm_search/postprocessing/analysis_of_result.py
@@ -108,7 +108,7 @@ def get_plots_from_result_of_analysis(SS_and_PTM_PSMs: pd.DataFrame, SS_peptides
 
 
     plt.figure(figsize=(15, 15))
-    sns.set(font_scale=0.85)
+    sns.set(font_scale=2.5)
     sns.set_context("paper")
     sns.set_style({'grid.color': '.1', 'grid.linestyle': ':'})
     plt.subplot(1, 1, 1)
@@ -123,7 +123,7 @@ def get_plots_from_result_of_analysis(SS_and_PTM_PSMs: pd.DataFrame, SS_peptides
     plt.ylabel("")
     plt.xlabel("Coverage of proteins (%)")
     plt.savefig(fdr_analysis_dir / f'{config.experiment_name}_increasing_coverage_of_peptides.png',
-                dpi=300, bbox_inches='tight')
+                dpi=100, bbox_inches='tight')
 
     # ------------------------------------------------------------------------------------------------------------------
     if config.search_mode == 'fast_search':
